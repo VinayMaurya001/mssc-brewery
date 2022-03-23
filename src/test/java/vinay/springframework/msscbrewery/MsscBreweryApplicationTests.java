d4fc9605b0906cd4f76e697091248c70650349d5
unknown <itsvinaymaurya@gmail.com>
2022-03-23 11:37:56 +0530

    First Commit

---
+++ src/test/java/vinay/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package vinay.springframework.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
